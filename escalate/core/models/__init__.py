6632103565bccb8178b74734b597a8f52439fb5e
Venkateswaran Shekar <vshekar@vshekar-19.local>
2019-08-06 15:39:02 -0400

    Changed requirements

--- escalate/core/models/__init__.py
+++ escalate/core/models/__init__.py
@@ -1,38 +1,9 @@
-from django.db import models
-
-class Document(models.Model):
-    description = models.CharField(max_length=255)
-    document = models.BinaryField()
-    doctype = models.CharField(max_length=255)
-    version = models.CharField(max_length=255)
-    alias = models.CharField(max_length=255)
-    add_dt = models.DateTimeField()
-    mod_dt = models.DateTimeField()
-
-class Note(models.Model):
-    notetext = models.CharField(max_length=255)
-    document = Document()
-    add_dt = models.DateTimeField()
-    mod_dt = models.DateTimeField()
-
-class CommonFields(models.Model):
-    note = models.ForeignKey(Note, on_delete=models.CASCADE)
-    alias = models.CharField(max_length=255)
-    add_dt = models.DateTimeField()
-    mod_dt = models.DateTimeField()
-    description = models.CharField(max_length=255)
-
-    class Meta:
-        abstract = True
-
-class Tag(CommonFields):
-    pass
-
-
-from .organization import *
-from .actions import *
-from .descriptors import *
-from .ingredients import *
-from .measures import *
-from .science import *
+from .common import *
 from .tracking import *
+from .science import *
+from .measures import *
+from .ingredients import *
+from .descriptors import *
+from .actions import *
+from .organization import *
+from django.db import models
