f6f13081b93fed674ce03a19a9df461863f0720d
unknown <gv.shekar@gmail.com>
2020-07-23 20:55:14 -0400

    Removed .idea folders

--- escalate/escalate/settings/base.py
+++ escalate/escalate/settings/base.py
@@ -144,5 +144,6 @@ REST_FRAMEWORK = {
     # 'django_filters.rest_framework.DjangoFilterBackend'
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 
 }
