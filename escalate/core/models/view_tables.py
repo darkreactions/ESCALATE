fb712244d173061652aa2f74cd3c95d17eece052
Shekar V <vshekar@haverford.edu>
2020-08-28 10:45:03 -0400

    Upgraded to Django 3.1, all other related packages also upgraded and modified in requirement.txt

--- escalate/core/models/view_tables.py
+++ escalate/core/models/view_tables.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
-
+#from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 managed_value = False
 
 
