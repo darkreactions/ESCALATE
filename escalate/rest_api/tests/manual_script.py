5951ba697c8d6a152895a8de0916e94b797af6ff
Shekar V <vshekar@haverford.edu>
2020-12-30 21:36:08 -0500

    Changed and installed autoprefetch to get 40% speedup

--- escalate/rest_api/tests/manual_script.py
+++ escalate/rest_api/tests/manual_script.py
@@ -6,13 +6,14 @@ import coreapi
 
 # outcome, workflow.workflow.step.workflow_object.action, bill_of_materials
 
-omit_cols = "?omit=edocs,tags,notes,add_date,mod_date,status"
-
+omit_cols = "omit=edocs,tags,notes,add_date,mod_date,status,status_description"
+expand_cols = "expand=bill_of_materials.bom_material"
+full_expand_cols = "expand=bill_of_materials.bom_material,workflow.step.workflow_object,outcome"
 
 def expand_list(url_list):
     data_list = []
     for url in url_list:
-        req = requests.get(url+omit_cols)
+        req = requests.get(f'{url}?{omit_cols}')
         bom_data = json.loads(req.text)
         data_list.append(bom_data)
     return data_list
@@ -20,7 +21,7 @@ def expand_list(url_list):
 
 def expand_instance(instance, col_name):
     url = instance[col_name]
-    req = requests.get(url+omit_cols)
+    req = requests.get(f'{url}?{omit_cols}')
     base_data = json.loads(req.text)
     instance[col_name] = base_data
     return instance
@@ -36,22 +37,23 @@ def expand_dict(data_dict, col_name):
 
 
 def run_all():
-    r = requests.get(
-        'http://localhost:8000/api/experiment/ad566ff8-f1ea-4416-ace5-7adb91112931/'+omit_cols)
+    r = requests.get(f'http://localhost:8000/api/experiment/cdc46d6b-e801-465f-87fc-c38b0ed7a7e1/?{expand_cols}&{omit_cols}')
 
     data = json.loads(r.text)
     data['outcome'] = expand_list(data['outcome'])
     data['workflow'] = expand_list(data['workflow'])
-    data['workflow'] = expand_dict(data['workflow'], ['workflow'])
-    data['bill_of_materials'] = expand_list(data['bill_of_materials'])
+    #data['workflow'] = expand_dict(data['workflow'], ['workflow'])
+    #data['bill_of_materials'] = expand_list(data['bill_of_materials'])
 
     for workflow in data['workflow']:
-        workflow['workflow']['step'] = expand_list(
-            workflow['workflow']['step'])
-        workflow['workflow']['step'] = expand_dict(
-            workflow['workflow']['step'], ['workflow_object', 'action'])
+        #workflow['step'] = expand_list(workflow['step'])
+        workflow['step'] = expand_dict(workflow['step'], ['workflow_object', 'action'])
+    
+    #data['bill_of_materials']['bom_material'] = expand_dict(data['bill_of_materials'], ['bom_material'])
+    
+
     json.dump(data, open('data_dump.json', 'w'), indent=4)
 
 
-print(timeit.timeit(lambda: run_all(), number=1))
-
+# print(timeit.timeit(lambda: run_all(), number=1))
+print(timeit.timeit(lambda: requests.get(f'http://localhost:8000/api/experiment/cdc46d6b-e801-465f-87fc-c38b0ed7a7e1/?{full_expand_cols}'), number=20))
