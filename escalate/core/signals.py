63b862b6327a2d4e7124c2a3c306530480bde87a
jpannizzo <jpannizzo@haverford.edu>
2021-05-28 10:35:26 -0400

    added upsert_bom_material_composite and upsert_workflow_action_set. need to update basic tests

--- escalate/core/signals.py
+++ escalate/core/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from core.models import Person, Actor, BomCompositeMaterial, CompositeMaterial, Measure, MeasureX, UdfX, Udf, \
-Property, PropertyX, Parameter, ParameterX, Action, ActionParameter
+Property, PropertyX, Parameter, ParameterX, Action, ActionParameter, WorkflowActionSet, Workflow, ExperimentWorkflow, \
+BomCompositeMaterial, BomMaterialIndex
+from core.models.view_tables.workflow import Experiment
 
 @receiver(post_save, sender=Person)
 def create_actor(sender, **kwargs):
@@ -46,7 +48,7 @@ def create_measure_x(sender, **kwargs):
     Creates measure_x table based on Measure table
     
     Args:
-        sender (Udf Instance): Instance of the newly created measure_x
+        sender (Measure Instance): Instance of the newly created measure_x
     """
     if kwargs['created']:
         measure_x = MeasureX(Measure=kwargs['instance'])
@@ -58,7 +60,7 @@ def create_property_x(sender, **kwargs):
         Creates property_x table based on property table
         
         Args:
-            sender (Udf Instance): Instance of the newly created property_x
+            sender (Property Instance): Instance of the newly created property_x
     """
 
     if kwargs['created']:
@@ -71,11 +73,11 @@ def create_parameter_x(sender, **kwargs):
         Creates parameter_x table based on parameter table
         
         Args:
-            sender (Udf Instance): Instance of the newly created parameterx
+            sender (Parameter Instance): Instance of the newly created parameterx
     """
 
     if kwargs['created']:
-        parameter_x = ParameterX(parameter=kwargs['instance'])
+        parameter_x = ParameterX(Parameter=kwargs['instance'])
     parameter_x.save()
 
 @receiver(post_save, sender=Action)  
@@ -84,9 +86,46 @@ def create_action_parameter(sender, **kwargs):
         Creates action_parameter table based on action table
         
         Args:
-            sender (Udf Instance): Instance of the newly created action_parameter
+            sender (Action Instance): Instance of the newly created action_parameter
     """
 
     if kwargs['created']:
-        action_parameter = ActionParameter(parameter=kwargs['instance'])
+        action_parameter = ActionParameter(Action=kwargs['instance'])
     action_parameter.save()
+
+"""
+Verify with tests:
+workflow_action_set ->
+    experiment
+    workflow
+    experiment_workflow
+"""    
+@receiver(post_save, sender=WorkflowActionSet)  
+def create_workflow_object(sender, **kwargs):
+    """
+        Creates experiment, workflow, experiment_workflow table based on workflowactionset table
+        
+        Args:
+            sender (WorkflowActionSet Instance): Instance of the newly created action_parameter
+    """
+
+    if kwargs['created']:
+        experiment = Experiment(WorkflowActionSet=kwargs['instance'])
+        workflow = Workflow(WorkflowActionSet=kwargs['instance'])
+        experiment_workflow = ExperimentWorkflow(WorkflowActionSet=kwargs['instance'])
+    experiment.save()
+    workflow.save()
+    experiment_workflow.save()
+
+@receiver(post_save, sender=BomCompositeMaterial)  
+def create_bom_material_index(sender, **kwargs):
+    """
+        Creates bom_material_index table based on bom_composite_material table
+        
+        Args:
+            sender (BomCompositeMaterial Instance): Instance of the newly created bom_material_index
+    """
+
+    if kwargs['created']:
+        bom_material_index = BomMaterialIndex(BomCompositeMaterial=kwargs['instance'])
+    bom_material_index.save()
