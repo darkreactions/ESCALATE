e02d8e5d8546cdd681d64dfe924ba60e7d5198cc
Shekar V <vshekar@haverford.edu>
2021-06-08 09:06:31 -0400

    Fixed reset script and new initial migration

--- escalate/build_django_db.sh
+++ escalate/build_django_db.sh
@@ -6,8 +6,7 @@ elif [[ "$1" = "reset" ]]; then
     python manage.py reset_schema
 fi
 
-# python manage.py reset_db
-# rm -r ./core/migrations/*
+rm -r ./core/migrations/00*.py
 python manage.py makemigrations
 python manage.py migrate
 python manage.py setup_users
