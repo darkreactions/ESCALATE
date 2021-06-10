07aaa46cf7a77cd4708605a24ca80d38f3cea121
Isaac <ichang1@haverford.edu>
2021-06-10 16:52:30 -0400

    added load loadtables command to db build script

--- escalate/build_django_db.sh
+++ escalate/build_django_db.sh
@@ -10,4 +10,5 @@ rm -r ./core/migrations/00*.py
 python manage.py makemigrations
 python manage.py migrate
 python manage.py load_standard
+python manage.py load_loadtables
 python manage.py setup_users
