ca9ccb3b5a333d75fc3b3f5b1632468e785c5864
Isaac <ichang1@haverford.edu>
2021-06-11 18:34:07 -0400

    added new management commands to build script

--- escalate/build_django_db.sh
+++ escalate/build_django_db.sh
@@ -10,5 +10,8 @@ rm -r ./core/migrations/00*.py
 python manage.py makemigrations
 python manage.py migrate
 python manage.py load_standard
+python manage.py load_organization
+python manage.py load_person
+python manage.py load_systemtool
 python manage.py load_loadtables
 python manage.py setup_users
