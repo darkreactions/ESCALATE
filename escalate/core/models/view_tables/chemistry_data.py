e59eab8b23d1e82d5007646e0bb047c80e4d95d4
jpannizzo <jpannizzo@haverford.edu>
2021-09-13 15:34:54 -0400

    update vessel instance, added contents table

--- escalate/core/models/view_tables/chemistry_data.py
+++ escalate/core/models/view_tables/chemistry_data.py
@@ -54,13 +54,27 @@ class Vessel(DateColumns, StatusColumn, ActorColumn, DescriptionColumn):
     def __str__(self):
         return "{}  {}".format(self.plate_name, self.well_number)
 
-class VesselObject(DateColumns, StatusColumn, ActorColumn, DescriptionColumn):
-    uuid = RetUUIDField(primary_key=True, default=uuid.uuid4, db_column='vessel_object_uuid')
+class VesselInstance(DateColumns, StatusColumn, ActorColumn, DescriptionColumn):
+    uuid = RetUUIDField(primary_key=True, default=uuid.uuid4, db_column='vessel_instance_uuid')
     model_uuid = models.ForeignKey('Vessel', on_delete=models.DO_NOTHING,
                               blank=True, null=True,
                               db_column='vessel_model_uuid',
-                              related_name='vessel_object_vessel_model_uuid')
-    contents = models.CharField(max_length=255,blank=True, null=True)
+                              related_name='vessel_instance_vessel_model_uuid')
+
+    def __str__(self):
+        return f'{self.description}'
+    
+class Contents(DateColumns, StatusColumn, ActorColumn, DescriptionColumn):
+    uuid = RetUUIDField(primary_key=True, default=uuid.uuid4, db_column='contents_uuid')
+    vessel_instance_uuid = models.ForeignKey('VesselInstance', on_delete=models.DO_NOTHING,
+                              blank=True, null=True,
+                              related_name='contents_vessel_instance_uuid')
+    base_bom_material = models.OneToOneField('BaseBomMaterial', on_delete=models.DO_NOTHING,
+                                blank=True, null=True,
+                                db_column='bom_material_uuid',
+                                related_name='contents_bom_material_uuid')
+    valtype = models.CharField(max_length=255,
+                               blank=True, null=True)
     
     def __str__(self):
         return f'{self.description}'
